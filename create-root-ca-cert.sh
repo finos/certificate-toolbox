7d1ff1404b8b7c0b13855476b33da92baf0cc0f3
Maurizio Pillitu <maoo@symphony.foundation>
2016-09-19 16:27:23 +0200

    first commit

---
+++ create-root-ca-cert.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Licensed to the Symphony Software Foundation (SSF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The SSF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+echo `date`
+echo "*********************************************************************"
+echo "**                                                                 **"
+echo "**          Create a Root CA Signing Certificate to import         **"
+echo "**          on a Symphony Pod and authenticate Symphony bots       **"
+echo "**                                                                 **"
+echo "*********************************************************************"
+
+mkdir -p ./root
+
+if [ -f openssl-root-ca.ini ]; then
+  echo "Found openssl-root-ca.ini file"
+else
+	cp openssl-root-ca.ini.sample openssl-root-ca.ini
+  echo "Copied openssl-root-ca.ini.sample to openssl-root-ca.ini"
+fi
+
+if [ -d certs ]; then
+  echo "Found certs folder"
+else
+  mkdir -p ./certs
+  touch ./certs/certindex.txt
+  echo "100000" > ./certs/serial
+fi
+
+if [ -f ./root/ca_signing_key.pem ]; then
+		echo "ERROR: Signing cert already exists in 'root/ca_signing_key.pem'"
+		echo "back up and/or delete before continuing"
+		exit -1
+fi
+echo 'Running: openssl req -new -x509 -extensions v3_ca -keyout root/ca_signing_key.pem -out root/ca_signing_cert.pem -days 3650 -config ./openssl-root-ca.ini'
+openssl req -new -x509 -extensions v3_ca -keyout root/ca_signing_key.pem -out root/ca_signing_cert.pem -days 3650 -config ./openssl-root-ca.ini
+
+if [ -f ./root/ca_signing_key.pem ]; then
+		echo "SUCCESS!"
+		echo "Back up 'root/ca_signing_key.pem' and 'root/ca_signing_cert.pem'"
+		echo "Do not share 'root/ca_signing_key.pem'"
+		echo "Import 'root/ca_signing_cert.pem' into the Managed Certificates dashboard of the Symphony Admin Console"
+fi
---
+++ create-user-cert.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Licensed to the Symphony Software Foundation (SSF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The SSF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+echo `date`
+echo "*********************************************************************"
+echo "**                                                                 **"
+echo "**          Create an X.509 (User) Identity Certificate            **"
+echo "**          for a Symphony Bot                                     **"
+echo "**                                                                 **"
+echo "*********************************************************************"
+
+mkdir -p ./users
+
+if [ -f openssl-user.ini ]; then
+  echo "Found openssl-user.ini file"
+else
+	cp openssl-user.ini.sample openssl-user.ini
+  echo "Copied openssl-user.ini.sample to openssl-user.ini"
+fi
+
+if [ -f root/ca_signing_key.pem ]; then
+  echo "Found root/ca_signing_key.pem file"
+else
+	echo "ERROR: Please create signing certificate by running 'make-root-ca-cert.sh'"
+	exit -1
+fi
+
+if [ -z "$1" ]; then
+	echo "ERROR: Please specify the certificate filename on the command line such as 'make-user-cert.sh botuser1'"
+	exit -1
+else
+	echo "Running: openssl req -new -nodes -out users/$1-req.pem -keyout users/$1-key.pem -days 3650 -config ./openssl-user.ini"
+	openssl req -new -nodes -out users/$1-req.pem -keyout users/$1-key.pem -days 3650 -config ./openssl-user.ini
+
+	echo "Running: openssl ca -out users/$1-cert.pem -days 3650 -config ./openssl-user.ini -infiles users/$1-req.pem"
+	openssl ca -out users/$1-cert.pem -days 3650 -config ./openssl-user.ini -infiles users/$1-req.pem
+
+	echo "Running: openssl pkcs12 -export -in users/$1-cert.pem -inkey users/$1-key.pem -certfile root/ca_signing_cert.pem -name $1 -out users/$1-cert.p12"
+	openssl pkcs12 -export -in users/$1-cert.pem -inkey users/$1-key.pem -certfile root/ca_signing_cert.pem -name $1 -out users/$1-cert.p12
+
+	if [ -f users/$1-cert.p12 ]; then
+    echo "Created signed certificate file users/$1-cert.p12 ; you can now (securely) distribute it to the owner of '$1' Service Account."
+  fi
+fi
